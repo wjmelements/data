800a5a8b739fc8b11dbbfa743b33cefdfbfc26c6
William Morriss <wjmelements@gmail.com>
2012-12-23 02:25:04 -0600

    Began linkedlist

---
+++ linkedlist.h
@@ -0,0 +1,34 @@
+#ifndef linkedlist_w
+#define linkedlist_w
+
+#include <cstddef>
+using std::size_t;
+template <typename T>
+class linkedlist
+{
+private:
+	template <typename E> class linkedlistnode
+	{
+	private:
+		E data;
+		linkedlistnode<E> prev;
+		linkedlistnode<E> next;
+	public:
+		linkedlistnode(E data);
+	};
+	size_t count;
+	linkedlistnode<T> first;
+	linkedlistnode<T> last;
+public:
+	linkedlist();
+	linkedlist(T first);
+	linkedlist(T* elements, size_t size);
+	size_t size(); // O(1)
+	T at(size_t index); // O(n)
+	void push_back(T append); // O(1)
+	void push_front(T prepend); // O(1)
+	T pop_back(); // O(1)
+	T pop_front(); // O(1)
+	bool empty(); // O(1)
+};
+#endif
