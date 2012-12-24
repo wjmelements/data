e3a6bdb6d5acd637ccb478dad4d71a0172405af3
William Morriss <wjmelements@gmail.com>
2012-12-23 22:12:18 -0600

    Implemented some basic functionality in linkedlist. There is a bug with push_back

--- linkedlist.h
+++ linkedlist.h
@@ -9,8 +9,8 @@ class linkedlist
 {
 private:
 	size_t count;
-	linkedlistnode<T> first;
-	linkedlistnode<T> last;
+	linkedlistnode<T>* first;
+	linkedlistnode<T>* last;
 public:
 	linkedlist();
 	linkedlist(T first);
@@ -23,4 +23,61 @@ public:
 	T pop_front(); // O(1)
 	bool empty(); // O(1)
 };
+
+template <typename T> linkedlist<T>::linkedlist()
+{
+	this->count = 0;
+	this->first = NULL;
+	this->last = NULL;
+}
+template <typename T> linkedlist<T>::linkedlist(T first)
+{
+	this->count = 1;
+	this->first = new linkedlistnode<T>(first);
+	this->last = this->first;
+}
+template <typename T> linkedlist<T>::linkedlist(T* elements, size_t size)
+{
+	if (size)
+	{
+		this->count = 1;
+		this->first = new linkedlistnode<T>(elements[0]);
+		this->last = this->first;
+		while(this->count < size)
+		{
+			push_back(elements[this->count]);
+		}
+	}
+	else
+	{
+		this->count = 0;
+		this->first = NULL;
+		this->last = NULL;
+	}
+}
+template <typename T> void linkedlist<T>::push_back(T elem)
+{
+	linkedlistnode<T>* pushed = new linkedlistnode<T>(this->last,elem);
+	if(this->count)
+	{
+		this->last->next = pushed;
+		this->last = pushed;
+	}
+	else
+	{
+		this->last = pushed;
+		this->first = this->last;
+	}
+	this->count++;
+}
+template <typename T> T linkedlist<T>::at(size_t index)
+{
+	linkedlistnode<T>* current = this->first;
+	while(index)
+	{
+		current = current->next;
+		index--;
+	}
+	return *(current->data);
+}
 #endif
--- test.cpp
+++ test.cpp
@@ -1,5 +1,30 @@
-#include "linkedlistnode.h" 
+#include "linkedlist.h" 
+#include <cstddef>
+#include <iostream>
+using namespace std;
 int main() {
 	int six = 6;
-	linkedlistnode<int> yay(six);
+	linkedlist<linkedlist<int> > temp;
+	linkedlist<int> sicks(6);
+	int size = 26;
+	char* alphabet = new char[size];
+	linkedlist<char> alpha2;
+	for(size_t letter = 0; letter < size; letter++)
+	{
+		alphabet[letter] = 'a' + letter;
+		alpha2.push_back(alphabet[letter]);
+		cout << alphabet[letter];
+	}
+	linkedlist<char> alpha(alphabet,size);
+	cout << '\n';
+	for(size_t index = 0; index < size; index++)
+	{
+		cout << alpha.at(index);
+	}
+	cout << '\n';
+	for(size_t index = 0; index < size; index++)
+	{
+		cout << alpha2.at(index);
+	}
+	cout << '\n';
 }
